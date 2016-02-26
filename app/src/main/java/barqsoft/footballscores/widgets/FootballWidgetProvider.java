d9f1bd54d45f7c8ad03fe542f4243fc0dd4d8baa
Suhyun Kim <Suhyun.Kim@viacommix.com>
2016-02-25 21:25:47 -0500

    Edited widget provider

--- app/src/main/java/barqsoft/footballscores/widgets/FootballWidgetProvider.java
+++ app/src/main/java/barqsoft/footballscores/widgets/FootballWidgetProvider.java
@@ -27,13 +27,22 @@ public class FootballWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         for (int appWidgetId : appWidgetIds) {
+            //Instantiate the Remoteviews object for the ap widget layout
             RemoteViews views = new RemoteViews(context.getPackageName(),
                     R.layout.widget_list_layout);
 
             Intent intent = new Intent(context, FootballWidgetService.class);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
             intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            // Set up the RemoteViews object to use a RemoteViews adapter.
+            // This adapter connects
+            // to a RemoteViewsService  through the specified intent.
+            // This is how you populate the data.
             views.setRemoteAdapter(appWidgetId, R.id.widget_list, intent);
+            // The empty view is displayed when the collection has no items.
+            // It should be in the same layout used to instantiate the RemoteViews
+            // object above.
             views.setEmptyView(R.id.widget_list, R.id.empty_view);
             appWidgetManager.updateAppWidget(appWidgetId, views);
             //starts service to do the heavy lifting
